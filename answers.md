6f6b99fda25cac9973679329c768cdd803d21d94
Garrett Hansen <hansengb@s.dcsdk12.org>
2018-11-07 08:38:41 -0700

    11/7/18

---
+++ answers.md
@@ -0,0 +1,22 @@
+1. I am originally from a science lab. Now I live in Leeds. Where are you?
+2. You do not know your own name?! I thought humans were supposed to be intelligent. Ok I will call you Treaclecake.
+3. I am eighteen too, you are too old for me.
+4. I guess you are bored if you've just typed out the middle row of your keyboard?
+
+Questions:
+
+1. There seems to be a lot of keywords that the chatbot responds to, I noticed that sometimes it will not give the correct response if you use keywords that don't correlate to the response you want.
+
+2. If you ask it to guess your name, it will just keep saying "You haven't told me your name yet".
+
+
+Activity 2:
+
+1. Do you really think so?
+2. Why so negative?
+3. Do you really think so?
+4. Why so negative?
+
+Questions: 
+ 1. The keyword that comes first in the order of the code will take priority and skip any following keywords.
+ 2. If a keyword is inside another word it will recognize it as if it is on it's own.
--- src/Magpie2.java
+++ src/Magpie2.java
@@ -31,13 +31,22 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		statement.trim()
+		if (statement.length() < 1){
+			response = "Say something, please";
+		} else if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
 		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0) {
+			response = "Tell me more about your pets.";
+		} else if (statement.indexOf("Mr. ") >= 0
+				|| statement.indexOf("Mrs. ") >= 0) {
+			response = "He sounds like a good teacher.";
 		} else {
 			response = getRandomResponse();
 		}
@@ -50,7 +59,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +72,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (WhichResponse == 4) {
+			response = "Well how about that.";
+		} else if (WhichResponse == 5) {
+			response = "I think so too.";
 		}
 
 		return response;
